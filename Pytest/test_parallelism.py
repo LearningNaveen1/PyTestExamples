caf50077e3d3625abbc63933ea9d596b312f48f7
Naveen <learning.naveen1@gmail.com>
2021-01-16 13:39:28 +0530

    parallel test program

---
+++ Pytest/test_parallelism.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_parallel_m1():
+    print("m1")
+
+def test_parallel_m2():
+    print("m2")
+
+def test_parallel_m3():
+    print("m3")
+
+def test_parallel_m4():
+    print("m4")
+
+def test_parallel_m5():
+    print("m5")
